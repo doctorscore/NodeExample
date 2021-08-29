cb2f6220bc2ff63df7215f4ce76b4c5854efc3e3
jagadeesh32 <katla.jagadeesh@gmail.com>
2021-08-29 10:05:10 +0530

    to test run in cloud shell

--- db/db.js
+++ db/db.js
@@ -6,7 +6,8 @@ dbconf = require('../config/db.json');
 
 var dbString = 'mongodb+srv://' + dbconf.dbcredentials.user;
 dbString = dbString + ':' + dbconf.dbcredentials.password;
-dbString = dbString + '@' + "cluster0.qphwc.mongodb.net/"+serverConfig.database+"?retryWrites=true&w=majority";
+// dbString = dbString + '@' + "cluster0.qphwc.mongodb.net/"+serverConfig.database+"?retryWrites=true&w=majority";
+dbString = dbString + '@' + "cluster0.y3kvk.mongodb.net"+serverConfig.database+"?retryWrites=true&w=majority";
 console.log('db string is '+dbString);
   
 function listen() {
